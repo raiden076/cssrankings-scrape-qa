ef1df1e58713ea2cec969b45f36b00de62e1476a
raiden <mail@raiden076.xyz>
2023-07-07 00:40:43 +0530

    Update Project
    Signed-off-by: raiden <mail@raiden076.xyz>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# edit
+Universities = {csrankings.org}
+Departments = {Mathematics, Computer Science, Electrical Engineering, EECS}
+Semesters = {Fall 2021, Spring 2022, Fall 2022, Spring 2023}
+Open = available online without robots.txt or with allow.
+pdfs with Q&A in U intersection D intersection S intersection O
--- csr/__main__.py
+++ csr/__main__.py
@@ -5,23 +5,27 @@ import json
 
 # Scrape csrankings.org for all the university names (there are 174 in US). May use bs4 or selenium or scrapy.
 # Make a list 👇 Here
-uni_names = []
+# uni_names = []
 
 # Copy the dep names and format them appropriately, from wa texts.
 #   here   👇
-uni_deps = []
+uni_deps = ["Mathematics, Computer Science, Electrical Engineering, EECS"]
 
-# all_urls = [] # for later
+# all_urls = [] # for lat
+queries = [
+    "Univ. of Illinois Computer Science CS questions and answers filetype:pdf",
+    # " filetype:pdf questions and answers",
+    # "Electrical Engineering filetype:pdf questions and answers",
+    # "EECS filetype:pdf questions and answers",
+]
 
 
-def app(i_tbs: str) -> None:
-    query = "filetype:pdf Univ. of Illinois at Urbana-Champaign CS Computer Science"
-    #         add 2 loop here {    👆 over uni names   }{    👆 over dept names}
+def app(q, i_tbs: str) -> None:
     client = yagooglesearch.SearchClient(
-        query,
+        q,
         tbs=i_tbs,
         max_search_result_urls_to_return=1000,
-        http_429_cool_off_time_in_minutes=45,
+        http_429_cool_off_time_in_minutes=1,
         http_429_cool_off_factor=1.5,
         # proxy="socks5h://127.0.0.1:9050",
         verbosity=5,
@@ -30,7 +34,6 @@ def app(i_tbs: str) -> None:
     client.assign_random_user_agent()
 
     urls = client.search()
-
     len(urls)
     # add the urls to the all_urls list
     with open("urls.txt", "w", encoding="utf-8") as f:
@@ -43,4 +46,5 @@ if __name__ == "__main__":
     r = yagooglesearch.get_tbs(
         from_date=datetime(2020, 8, 31), to_date=datetime(2023, 6, 2)
     )
-    app(r)
+    for query in queries:
+        app(query, r)
