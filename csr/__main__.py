b8ae268a46b3ab0879022301f59e129a6f6f27e6
raiden <mail@raiden076.xyz>
2023-07-06 15:54:09 +0530

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   csr/__init__.py 	new file:   csr/__main__.py 	modified:   poetry.lock 	modified:   pyproject.toml 	new file:   yagooglesearch.py.log

---
+++ csr/__main__.py
@@ -0,0 +1,42 @@
+import yagooglesearch
+from datetime import datetime
+
+# Scrape csrankings.org for all the university names (there are 174 in US). May use bs4 or selenium or scrapy.
+# Make a list 👇 Here
+uni_names = []
+
+# Copy the dep names and format them appropriately, from wa texts.
+#   here   👇
+uni_deps = []
+
+
+def app(i_tbs: str) -> None:
+    query = "filetype:pdf Carnegie Mellon University CS Computer Science"
+    #         add 2 loop here {    👆 over uni names   }{ 👆 over dept names}
+    client = yagooglesearch.SearchClient(
+        query,
+        tbs=i_tbs,
+        max_search_result_urls_to_return=1000,
+        http_429_cool_off_time_in_minutes=45,
+        http_429_cool_off_factor=1.5,
+        # proxy="socks5h://127.0.0.1:9050",
+        verbosity=5,
+        verbose_output=True,  # False (only URLs) or True (rank, title, description, and URL)
+    )
+    client.assign_random_user_agent()
+
+    urls = client.search()
+
+    len(urls)
+
+    for url in urls:
+        print(url)
+
+
+# Change the print. Return a json dumps. Or just directly write to a json (NOT CSV, we still have work left)
+
+if __name__ == "__main__":
+    r = yagooglesearch.get_tbs(
+        from_date=datetime(2020, 8, 31), to_date=datetime(2023, 6, 2)
+    )
+    app(r)
